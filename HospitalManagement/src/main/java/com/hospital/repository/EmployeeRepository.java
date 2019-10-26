10c16c08d8c922253d7ec8af5110b731546e1a50
Mac24h <autum7k@yahoo.com>
2019-10-26 17:16:54 +0700

    Phuong da so sanh da test

--- HospitalManagement/src/main/java/com/hospital/repository/EmployeeRepository.java
+++ HospitalManagement/src/main/java/com/hospital/repository/EmployeeRepository.java
@@ -12,14 +12,19 @@ import java.util.List;
 import java.util.Set;
 
 public interface EmployeeRepository extends JpaRepository<Employee, Long>, EmployeeRepositoryCustom {
+    @Query("SELECT e FROM Employee e WHERE e.status = 1")
+    public List<Employee> findEmployeesActive(@Param("id") int speciality_id);
     @Query("SELECT s FROM Symptom s")
     public Set<Symptom> findAllSymptom();
     @Query("SELECT et FROM ExaminationType et")
     public Set<ExaminationType> findAllExamination();
     @Query("SELECT e FROM Employee e WHERE e.specialityId = :id")
-    public List<Employee> findEmployeesBySpeciality(@Param("id") Long speciality_id);
+    public List<Employee> findEmployeesBySpeciality(@Param("id") int speciality_id);
     @Query("SELECT e FROM ExaminationType et LEFT JOIN Employee e ON et.specialityId = e.specialityId" +
             " WHERE et.id = :id")
     public List<Employee> findExaminatorsByExaminationType(@Param("id") long examination_type_id);
-
+    @Query("SELECT s FROM Symptom s where s.symptomTypeId = 1")
+    public List<Symptom> fillSymptomTypeOne();
+    @Query("SELECT s FROM Symptom s where s.symptomTypeId = 2")
+    public List<Symptom> fillSymptomTypeTwo();
 }

5e6240fabbf9c26be57fe70bb9cbad566d1923e4
bac.nh88 <bac.nh88@gmail.com>
2019-10-26 16:36:29 +0700

    commit remain

--- HospitalManagement/src/main/java/com/hospital/controller/AdminController.java
+++ HospitalManagement/src/main/java/com/hospital/controller/AdminController.java
@@ -12,16 +12,23 @@ import java.util.List;
 
 import javax.validation.Valid;
 
-import com.hospital.model.Employee;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.ModelAndView;
 
+import com.hospital.model.Employee;
 import com.hospital.model.Position;
 import com.hospital.model.Speciality;
 import com.hospital.repository.EmployeeRepository;
